6e5a3f864a396bbe32a5361d07e877522c2baaab
Maureen <murimimaureen69@gmail.com>
2020-05-09 01:43:01 +0300

    Add Register and login class

---
+++ app/auth/views.py
@@ -0,0 +1,44 @@
+from flask import Blueprint
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+from app import db, bcrypt
+from app.models import User, Post, Comment
+from app.auth.forms import RegistrationForm, LoginForm
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data , password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html',title='Create New Account', form =form)
+    
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email= form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Unsuccessful login. Please check your email and password', 'danger')
+    return render_template('login.html',title='Log In', form =form)
+    
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,15 +1,15 @@
-# import unittest
-# from app.models import Post, User, Comment
+import unittest
+from app.models import Post, User, Comment
 
-# class TestPost(unittest.TestCase):
+class TestPost(unittest.TestCase):
     
-#    def setUp(self):
-#         self.user_1 = User(username= 'maureen',password = 'love234r',
-#                               email = 'nimo@gmail.com')
-#         self.new_post = Post(post_title = 'Blog',post_content = 'My First Blog', user_id = self.user_1.id)
-#         self.new_comment = Comment(comment = 'Best app ever',post_id = self.new_post.id, user_id = self.user_1.id)
+   def setUp(self):
+        self.user_1 = User(username= 'maureen',password = 'love234r',
+                              email = 'nimo@gmail.com')
+        self.new_post = Post(post_title = 'Blog',post_content = 'My First Blog', user_id = self.user_1.id)
+        self.new_comment = Comment(comment = 'Best app ever',post_id = self.new_post.id, user_id = self.user_1.id)
 
-#    def test_instance(self):
-#         self.assertTrue(isinstance(self.user_1, User))
-#         self.assertTrue(isinstance(self.new_post, Post))
-#         self.assertTrue(isinstance(self.new_comment, Comment))
+   def test_instance(self):
+        self.assertTrue(isinstance(self.user_1, User))
+        self.assertTrue(isinstance(self.new_post, Post))
+        self.assertTrue(isinstance(self.new_comment, Comment))
