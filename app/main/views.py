467d092fc77fded876a89ea73ce9ef7476df9954
Maureen <murimimaureen69@gmail.com>
2020-05-09 12:44:11 +0300

    Create new post file

--- app/main/views.py
+++ app/main/views.py
@@ -88,7 +88,7 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('main.home'))
-    return render_template('pitch.html', title='New Post',form=form, legend='New Post')
+    return render_template('new_post.html', title='New Post',form=form, legend='New Post')
     
   
 
