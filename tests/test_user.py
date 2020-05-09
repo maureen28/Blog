60f757a0263f9383ee039584d832f08a59f022de
Maureen <murimimaureen69@gmail.com>
2020-05-09 12:38:38 +0300

    Add error file and error function

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import Post, User, Comment
+from app.models import Post, User, Comment, Quote
 
 class TestPost(unittest.TestCase):
     
@@ -13,3 +13,17 @@ class TestPost(unittest.TestCase):
         self.assertTrue(isinstance(self.user_1, User))
         self.assertTrue(isinstance(self.new_post, Post))
         self.assertTrue(isinstance(self.new_comment, Comment))
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.random_quote = Quote("Nimo", "Your beautiful")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "Nimo")
+        self.assertEqual(self.random_quote.quote,"Your beautiful")
